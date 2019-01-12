2daa37c661132d39e709c3618488d9c307521f80
mnvro <mnvro@hotmail.com>
2019-01-12 15:17:57 +0100

    toString et getter et setter ajoutés

--- cours01/Animal.java
+++ cours01/Animal.java
@@ -1,9 +1,20 @@
 package cours01;
 
 public class Animal {
+	
+
 	private String espece;
 	private int age;
 	private double poids;
+
+	public Animal(String espece, int age, double poids) {
+		this.espece = espece;
+		this.age = age;
+		this.poids = poids;
+	}
+	
+	public Animal() {
+	}
 
 	public void dort() {
 		System.out.println("Je dors");
@@ -15,6 +26,34 @@ public class Animal {
 		System.out.println("Je mange");
 	}
 	
+	public String toString() {
+		return "Animal [espece=" + espece + ", age=" + age + " ans, poids=" + poids + "kg]";
+	}
+
+	public String getEspece() {
+		return espece;
+	}
+
+	public void setEspece(String espece) {
+		this.espece = espece;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public double getPoids() {
+		return poids;
+	}
+
+	public void setPoids(double poids) {
+		this.poids = poids;
+	}
+
 	public static void main(String[] args) {
 		
 		Animal felix = new Animal();
--- cours01/MainAnimal.java
+++ cours01/MainAnimal.java
@@ -4,8 +4,17 @@ public class MainAnimal {
 
 	public static void main(String[] args) {
 		Animal felix = new Animal("chat", 5, 3);
+		System.out.println(felix);
 		
-		Animal milou = new Animal("chien",7,8);
+		
+		//Animal milou = new Animal("chien",7,8);
+		Animal milou = new Animal();
+		milou.setEspece("chien");
+		milou.setAge(7);
+		milou.setPoids(8);
+		
+		System.out.println(milou.getEspece()+" "+milou.getAge()+" ans "+milou.getPoids()+" kg");
+		System.out.println(milou);
 
 	}
 
